c214b085dea8007f53e895ba854bef10e30a3d44
guptaRishitaa <rishitagupta2700@gmail.com>
2025-02-21 00:24:41 -0500

    notifications, billing, settings skeleton created, dark/light theme completed

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -142,3 +142,77 @@ export const searchUsers = async (query: string)=>{
         
     }
 }
+
+export const getPaymentInfo = async() => {
+    try {
+        const user = await currentUser()
+        if(!user) return {status : 404}
+
+        const payment = await client.user.findUnique({
+            where : {
+                clerkid : user.id
+            },
+            select : {
+                subscription :{
+                    select :{
+                        plan : true,
+                    }
+                }
+            }
+        })
+
+        if(payment) {
+            return {status : 200, data : payment}
+        }
+    } catch (error) {
+        return {status : 400}
+    }
+}
+
+export const getFirstView = async() => {
+    try {
+        const user = await currentUser()
+
+        if(!user) return {status : 404}
+
+        const userData = await client.user.findUnique({
+            where:{
+                clerkid : user.id
+            },
+            select : {
+                firstView : true
+            }
+        })
+
+        if(userData) {
+            return {status : 200, data : userData.firstView}
+        }
+        return {status : 400, data : false}
+    } catch (error) {
+        return {status : 400}
+    }
+}
+
+export const enableFirstView = async (state : boolean) => {
+    try {
+        const user = await currentUser()
+
+        if(!user) return {status : 404}
+
+        const view = await client.user.update({
+            where : {
+                clerkid : user.id
+            },
+            data:{
+                firstView : state
+            }
+
+        })
+
+        if(view) {
+            return {status : 200, data : 'Setting updated'}
+        }
+    } catch (error) {
+        return {status : 400}
+    }
+}
